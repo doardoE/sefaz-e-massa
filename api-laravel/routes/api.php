19d256df7a7a1f31a949609d9d4301f702506276
Eduardo Pereira <doardoperera@gmail.com>
2025-11-07 11:48:06 -0300

    feat (routes): Criação de rotas base.

--- api-laravel/routes/api.php
+++ api-laravel/routes/api.php
@@ -1,11 +1,14 @@
 <?php
 
-use Illuminate\Https\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\api;
+use App\Http\Controllers\api\ArrecadacoesController;
 
 // Route::middleware('auth:sanctum')->get('/user', function(Request $request) {
 //     return $request->$user();
 // });
 
-Route::get('/Arrecadacoes', [Arrecadacoes::class], index)->name('arrecadacoes.index');
+Route::get('/arrecadacoes', [ArrecadacoesController::class, 'index']);
+ROute::post('/arrecadacoes', [ArrecadacoesController::class, 'store']);
+Route::put('/arrecadacoes/{id}', [ArrecadacoesController::class, 'update']);
+Route::delete('/arrecadacoes/{id}', [ArrecadacoesController::class, 'destroy']);
+Route::get('/arrecadacoes/{id}', [ArrecadacoesController::class, 'show']);
