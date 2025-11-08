0c71bc52439453bbf79d051439311ae13a7df939
Eduardo Pereira <doardoperera@gmail.com>
2025-11-08 13:40:18 -0300

    feat (front-vue): criação do projeto front com Vue.js

---
+++ front-vue/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { Camera } from 'lucide-vue-next';
+</script>
+
+<template>
+  <div class="p-6 bg-blue-500 text-white text-center rounded-xl">
+    Tailwind está funcionando 🎉
+  </div>
+  lucide está funcionando!
+  <Camera color="red" :size="32" />
+</template>
